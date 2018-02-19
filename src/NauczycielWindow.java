d878354d7c28c907d192338a3c9baf15c5e39313
jb <jb4bfad@gmail.com>
2018-02-19 20:33:56 +0100

    wyswietla nb + DB

--- src/NauczycielWindow.java
+++ src/NauczycielWindow.java
@@ -48,7 +48,6 @@ public void odswiez(){
             tabelaNauczyciel.getModel().setValueAt(rs.getString("Imie"), i, columny);
             tabelaNauczyciel.getModel().setValueAt(rs.getString("Nazwisko"), i, columny+1);
                             Statement statement2 =db.conn.createStatement();
-                            //System.out.println("id= "+ rs.getString("ID")+"IDP="+  (wybierzPrzedmiot.getSelectedIndex()+1) );
                             String queryString2 = "select Ocena from Oceny where IDUczen="+rs.getString("ID")+" and IDPrzedmiot="+(wybierzPrzedmiot.getSelectedIndex()+1);
                             ResultSet rs2 = statement2.executeQuery(queryString2);
                             int j=0;
@@ -61,10 +60,7 @@ public void odswiez(){
         
     }catch(Exception e){
             e.printStackTrace();
-     }
-        
-        
-  
+     }  
     }
 
     /**
@@ -97,6 +93,16 @@ public void odswiez(){
         TabelaDodajOceny = new javax.swing.JTable();
         ZapiszOceneButton = new javax.swing.JButton();
         DodajText2 = new javax.swing.JTextField();
+        DodajNieobecnoscWindow = new javax.swing.JFrame();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        TabelaDodaNieobecnosc = new javax.swing.JTable();
+        DodajText3 = new javax.swing.JTextField();
+        ZapiszNieobecnoscButton = new javax.swing.JButton();
+        NieobecnosciWindow = new javax.swing.JFrame();
+        jLabel3 = new javax.swing.JLabel();
+        DodajNieobenoscButton = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        TabelaNieobecnosc = new javax.swing.JTable();
         obecnosciButton = new javax.swing.JButton();
         ocenyButton = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
@@ -263,6 +269,11 @@ public void odswiez(){
         });
 
         DodajText2.setEditable(false);
+        DodajText2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DodajText2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout DodajOcenyWindowLayout = new javax.swing.GroupLayout(DodajOcenyWindow.getContentPane());
         DodajOcenyWindow.getContentPane().setLayout(DodajOcenyWindowLayout);
@@ -287,6 +298,157 @@ public void odswiez(){
                 .addComponent(ZapiszOceneButton)
                 .addGap(42, 42, 42)
                 .addComponent(DodajText2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        DodajNieobecnoscWindow.setSize(new java.awt.Dimension(400, 317));
+
+        TabelaDodaNieobecnosc.setAutoCreateRowSorter(true);
+        TabelaDodaNieobecnosc.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID Ucznia", "Imie", "Nazwisko", "Data"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, true
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        TabelaDodaNieobecnosc.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_LAST_COLUMN);
+        TabelaDodaNieobecnosc.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jScrollPane3.setViewportView(TabelaDodaNieobecnosc);
+
+        DodajText3.setEditable(false);
+        DodajText3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DodajText3ActionPerformed(evt);
+            }
+        });
+
+        ZapiszNieobecnoscButton.setText("Zapisz");
+        ZapiszNieobecnoscButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ZapiszNieobecnoscButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout DodajNieobecnoscWindowLayout = new javax.swing.GroupLayout(DodajNieobecnoscWindow.getContentPane());
+        DodajNieobecnoscWindow.getContentPane().setLayout(DodajNieobecnoscWindowLayout);
+        DodajNieobecnoscWindowLayout.setHorizontalGroup(
+            DodajNieobecnoscWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(DodajNieobecnoscWindowLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(DodajNieobecnoscWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(DodajText3)
+                    .addGroup(DodajNieobecnoscWindowLayout.createSequentialGroup()
+                        .addGroup(DodajNieobecnoscWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(ZapiszNieobecnoscButton)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 5, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        DodajNieobecnoscWindowLayout.setVerticalGroup(
+            DodajNieobecnoscWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(DodajNieobecnoscWindowLayout.createSequentialGroup()
+                .addContainerGap(64, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(ZapiszNieobecnoscButton)
+                .addGap(18, 18, 18)
+                .addComponent(DodajText3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        NieobecnosciWindow.setSize(new java.awt.Dimension(550, 300));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("Nieobecność na zajęciach:");
+
+        DodajNieobenoscButton.setText("Dodaj");
+        DodajNieobenoscButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DodajNieobenoscButtonActionPerformed(evt);
+            }
+        });
+
+        TabelaNieobecnosc.setAutoCreateRowSorter(true);
+        TabelaNieobecnosc.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID Ucznia", "Imie", "Nazwisko", "Data"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, true
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        TabelaNieobecnosc.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_LAST_COLUMN);
+        TabelaNieobecnosc.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jScrollPane4.setViewportView(TabelaNieobecnosc);
+
+        javax.swing.GroupLayout NieobecnosciWindowLayout = new javax.swing.GroupLayout(NieobecnosciWindow.getContentPane());
+        NieobecnosciWindow.getContentPane().setLayout(NieobecnosciWindowLayout);
+        NieobecnosciWindowLayout.setHorizontalGroup(
+            NieobecnosciWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(NieobecnosciWindowLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(NieobecnosciWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+                    .addGroup(NieobecnosciWindowLayout.createSequentialGroup()
+                        .addGroup(NieobecnosciWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(DodajNieobenoscButton))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        NieobecnosciWindowLayout.setVerticalGroup(
+            NieobecnosciWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(NieobecnosciWindowLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
+                .addComponent(DodajNieobenoscButton)
+                .addGap(25, 25, 25))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -349,7 +511,30 @@ public void odswiez(){
 
     private void obecnosciButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_obecnosciButtonActionPerformed
         // TODO add your handling code here:
-        //asda
+        String daty="";
+        TabelaNieobecnosc.getColumnModel().getColumn(0).setPreferredWidth(30);
+        TabelaNieobecnosc.getColumnModel().getColumn(3).setPreferredWidth(300);
+        NieobecnosciWindow.setVisible(true);
+          try{
+            statement =db.conn.createStatement();
+            queryString = "select ID, Imie, Nazwisko from Uczniowie";
+            rs = statement.executeQuery(queryString);            
+            for(int i=0;rs.next();i++ ){
+                TabelaNieobecnosc.getModel().setValueAt(rs.getString("ID"), i, 0);
+                TabelaNieobecnosc.getModel().setValueAt(rs.getString("Imie"), i, 1);
+                TabelaNieobecnosc.getModel().setValueAt(rs.getString("Nazwisko"), i, 2);
+                Statement statement2 =db.conn.createStatement();
+                String queryString2 = "select Nieobecnosc from Obecnosc where IDUczen="+rs.getString("ID");
+                ResultSet rs2 = statement2.executeQuery(queryString2);
+                for(int j=0;rs2.next();j++ ){
+                    daty=daty+rs2.getString("Nieobecnosc")+" | ";
+                }
+                TabelaNieobecnosc.getModel().setValueAt(daty, i, 3);
+                daty="";
+            }      
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }//GEN-LAST:event_obecnosciButtonActionPerformed
 
     private void ocenyButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ocenyButtonActionPerformed
@@ -363,7 +548,6 @@ public void odswiez(){
 
     private void OdswiezOcenyButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OdswiezOcenyButtonActionPerformed
         // TODO add your handling code here:
-        
         this.odswiez();
     }//GEN-LAST:event_OdswiezOcenyButtonActionPerformed
 
@@ -390,8 +574,6 @@ public void odswiez(){
         }catch(Exception e){
             e.printStackTrace();
         }
-        
-        
     }//GEN-LAST:event_DodajOceneButtonActionPerformed
 
     private void ZapiszOceneButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ZapiszOceneButtonActionPerformed
@@ -434,14 +616,9 @@ public void odswiez(){
              
        
         }catch(Exception e){
-            e.printStackTrace();
-            
-        }
-                
-        this.odswiez();
-        
-     
-        
+            e.printStackTrace();  
+        }        
+        this.odswiez(); 
     }//GEN-LAST:event_ZapiszOceneButtonActionPerformed
 
     private void EdytujOcenyButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EdytujOcenyButtonActionPerformed
@@ -482,11 +659,7 @@ public void odswiez(){
             ResultSet rs2 = statement2.executeQuery(queryString2);
                 int j=0;
                 while (rs2.next()) {
-                     //System.out.println(tabelaNauczyciel.getModel().getValueAt(i, columny+2+j).toString());
-//                    if(Integer.parseInt(tabelaNauczyciel.getModel().getValueAt(i, columny+2+j).toString())==8)
-//                        System.out.println(tabelaNauczyciel.getModel().getValueAt(i, columny+2+j).toString());
                     if(tabelaNauczyciel.getModel().getValueAt(i, columny+2+j).equals("")) {
-//                        System.out.println("asdsa");
                             queryString = "DELETE FROM Oceny WHERE ID="+rs2.getString("ID");        
                             db.statement.executeUpdate(queryString);
                     }   
@@ -498,6 +671,70 @@ public void odswiez(){
         } 
         this.odswiez();    
     }//GEN-LAST:event_UsunButtonActionPerformed
+
+    private void DodajText2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DodajText2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DodajText2ActionPerformed
+
+    private void DodajText3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DodajText3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DodajText3ActionPerformed
+
+    private void ZapiszNieobecnoscButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ZapiszNieobecnoscButtonActionPerformed
+        // TODO add your handling code here:
+        DodajText3.setText("Poszę podać datę w formacie dd/mm/rrrr");
+        int i=0;
+        try{
+            Statement statement2 =db.conn.createStatement();      
+            queryString = "select ID, Imie, Nazwisko from Uczniowie";
+            ResultSet rs2 = statement2.executeQuery(queryString);
+            Statement statement1 =db.conn.createStatement(); 
+            i=0;
+            while (rs2.next()) {
+                if( TabelaDodaNieobecnosc.getModel().getValueAt(i, 3)!=null ){
+                    queryString="SELECT MAX(ID) ID FROM Obecnosc";
+                    ResultSet rs1 = statement1.executeQuery(queryString);
+                    rs1.next();
+                     int a=0;
+                     a= Integer.parseInt(rs1.getString("ID"))+1;
+                     queryString = "INSERT INTO Obecnosc (ID, IDUczen, Nieobecnosc) VALUES ("
+                             +a+","
+                             + TabelaDodaNieobecnosc.getModel().getValueAt(i, 0)+",'"
+                             + TabelaDodaNieobecnosc.getModel().getValueAt(i, 3)+"')";
+                     db.statement.executeUpdate(queryString);
+                     TabelaDodaNieobecnosc.getModel().setValueAt(null, i, 3);
+                     i++;
+                     }else{
+                        //DodajText2.setText("Nie wprowadzono wartości");
+                        i++;
+                        continue;
+                     }
+                
+               }
+            DodajNieobecnoscWindow.dispose();
+        }catch(Exception e){
+            e.printStackTrace();
+        }   
+        this.odswiez();
+    }//GEN-LAST:event_ZapiszNieobecnoscButtonActionPerformed
+
+    private void DodajNieobenoscButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DodajNieobenoscButtonActionPerformed
+        // TODO add your handling code here:
+        DodajNieobecnoscWindow.setVisible(true);
+        try{
+            statement =db.conn.createStatement();
+            queryString = "select ID, Imie, Nazwisko from Uczniowie";
+            rs = statement.executeQuery(queryString);            
+            for(int i=0;rs.next();i++ ){
+                TabelaDodaNieobecnosc.getModel().setValueAt(rs.getString("ID"), i, 0);
+                TabelaDodaNieobecnosc.getModel().setValueAt(rs.getString("Imie"), i, 1);
+                TabelaDodaNieobecnosc.getModel().setValueAt(rs.getString("Nazwisko"), i, 2);
+            }      
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        DodajText3.setText("Poszę podać datę w formacie dd/mm/rrrr");
+    }//GEN-LAST:event_DodajNieobenoscButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -535,19 +772,29 @@ public void odswiez(){
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JFrame DodajNieobecnoscWindow;
+    private javax.swing.JButton DodajNieobenoscButton;
     private javax.swing.JButton DodajOceneButton;
     private javax.swing.JFrame DodajOcenyWindow;
     private javax.swing.JTextField DodajText2;
+    private javax.swing.JTextField DodajText3;
     private javax.swing.JButton EdytujOcenyButton;
+    private javax.swing.JFrame NieobecnosciWindow;
     private javax.swing.JFrame OcenyWindow;
     private javax.swing.JButton OdswiezOcenyButton;
+    private javax.swing.JTable TabelaDodaNieobecnosc;
     private javax.swing.JTable TabelaDodajOceny;
+    private javax.swing.JTable TabelaNieobecnosc;
     private javax.swing.JButton UsunButton;
+    private javax.swing.JButton ZapiszNieobecnoscButton;
     private javax.swing.JButton ZapiszOceneButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JButton obecnosciButton;
     private javax.swing.JButton ocenyButton;
     private javax.swing.JTable tabelaNauczyciel;
